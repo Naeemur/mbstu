0e87da629e4e726711d3541fd82e7e262de8ad97
Md. Naeemur Rahman <nrnflash@gmail.com>
2017-03-06 15:51:39 +0600

    Fixed gallery bug

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(window).on('load', function(){
     var wookmark1 = new Wookmark('#wookmark1', {
         outerOffset: 10, // Optional, the distance to the containers border
         itemWidth: 210 // Optional, the width of a grid item
